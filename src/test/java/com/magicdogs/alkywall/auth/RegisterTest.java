8afb6047d241f70bf7c56ae1e6e9aa6d5f1903f7
janaroz <rozenbergjana@gmail.com>
2024-05-29 15:31:29 -0300

    agrego dos mini test mas

--- src/test/java/com/magicdogs/alkywall/auth/RegisterTest.java
+++ src/test/java/com/magicdogs/alkywall/auth/RegisterTest.java
@@ -26,6 +26,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Optional;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -51,7 +52,8 @@ public class RegisterTest {
     private SecurityService securityService;
 
     @InjectMocks
-    private SecurityController registerController;
+    @Autowired
+    private SecurityController securityController;
 
     @Mock
     private UserRepository userRepository;
@@ -74,26 +76,27 @@ public class RegisterTest {
     // TODO: validaciones DE REGISTRO
     @BeforeEach
     void setUp() {
-        validUser = new UserRegisterDTO("jana","roz", "tirar@tirar.com", "1234" );
+        validUser = new UserRegisterDTO("jana","roz", "tirar4@tirar.com", "1234" );
         usuarioVacio = new UserRegisterDTO("tiras","tirar","tirar@tirar.com","1234");
 
-        invalidUser = new UserRegisterDTO("vicky2","appleido123", "tulindamama.com", "1234");
+        invalidUser = new UserRegisterDTO("vicky2","appleido123", "tirar3@tirar.com", "1234");
         //invalidUser2 = new UserRegisterDTO("tom", "peresoso", "peresoso.com", "1234");
         MockitoAnnotations.openMocks(this);
 
     }
+    // por los datos de prueba va a saltar error, pero funciona
     @Test
     void whenValidInput_thenReturns201() throws Exception {
 
-        when(securityService.registerUser(any(UserRegisterDTO.class))).thenReturn(usuarioVacio);
+        //when(securityService.registerUser(any(UserRegisterDTO.class))).thenReturn(usuarioVacio);
 
-        when(securityService.login(any())).thenReturn("fake-jwt-token");
+        //when(securityService.login(any())).thenReturn("fake-jwt-token");
 
         mockMvc.perform(post("/auth/register")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(validUser)))
                 .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.email").value("tirar@tirar.com"));
+                .andExpect(jsonPath("$.email").value(validUser.getEmail()));
     }
 
     @Test
@@ -102,7 +105,31 @@ public class RegisterTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(invalidUser)))
                 .andExpect(status().isBadRequest());
+
+
     }
+    @Test
+    void nameEmpty() throws Exception {
+        invalidUser = new UserRegisterDTO("","apellido", "tulinda@mama.com", "1234");
+        mockMvc.perform(post("/auth/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(invalidUser)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$").value("El nombre no puede estar en blanco"));
+        //"El nombre no puede estar vacío"
+    }
+    @Test
+    void nameNotNull() throws Exception {
+        invalidUser = new UserRegisterDTO(null,"apellido", "tulinda@mama.com", "1234");
+        mockMvc.perform(post("/auth/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(invalidUser)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$").value("El nombre no puede estar vacío"));
+        //"El nombre no puede ser nulo"
+    }
+
+
 
     @Test
     void whenEmailAlreadyExists_thenReturns400() throws Exception {
@@ -115,10 +142,6 @@ public class RegisterTest {
                 .andExpect(jsonPath("$").value("User already exists"));
     }
 
-    @Test
-    void probando() throws Exception{
-
-    }
     @Test
     void registerShouldCreateUserAndAccounts() {
         when(userRepository.findByEmail("john@example.com")).thenReturn(Optional.empty());
@@ -136,11 +159,17 @@ public class RegisterTest {
         securityService.registerUser(registerRequest);
 
         // Assert
-        verify(userRepository, times(1)).findByEmail("john@example.com").get();
-        verify(roleRepository, times(1)).findByName(RoleNameEnum.USER).get();
+        verify(userRepository, times(1)).findByEmail("john@example.com");
+        verify(roleRepository, times(1)).findByName(RoleNameEnum.USER);
         verify(accountService, times(2)).generateUniqueCbu();
         verify(accountRepository, times(1)).save(argThat(account -> account.getCurrency() == CurrencyType.ARS && account.getUser().getEmail().equals("john@example.com")));
         verify(accountRepository, times(1)).save(argThat(account -> account.getCurrency() == CurrencyType.USD && account.getUser().getEmail().equals("john@example.com")));
         verify(userRepository, times(1)).save(any(User.class));
+    }
+
+    // TEST 29/05/2024
+    @Test
+    void contextLoads() throws Exception{
+        assertThat(securityController).isNotNull();
     }
 }
