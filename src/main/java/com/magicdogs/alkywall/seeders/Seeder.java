2c62329528b9f9cf0e26eab8aa11ff6330e920e5
Alan Ades <alan.ades2@gmail.com>
2024-05-31 10:52:54 -0300

    Realizando seed de Transaccion

--- src/main/java/com/magicdogs/alkywall/seeders/Seeder.java
+++ src/main/java/com/magicdogs/alkywall/seeders/Seeder.java
@@ -1,28 +1,37 @@
 package com.magicdogs.alkywall.seeders;
 
-import com.magicdogs.alkywall.entities.RoleNameEnum;
+import com.magicdogs.alkywall.Constants;
+import com.magicdogs.alkywall.entities.*;
+import com.magicdogs.alkywall.repositories.AccountRepository;
+import com.magicdogs.alkywall.repositories.TransactionRepository;
+import com.magicdogs.alkywall.servicies.AccountService;
 import lombok.AllArgsConstructor;
 import net.datafaker.Faker;
-import com.magicdogs.alkywall.entities.Role;
-import com.magicdogs.alkywall.entities.User;
 import com.magicdogs.alkywall.repositories.RoleRepository;
 import com.magicdogs.alkywall.repositories.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Locale;
+import java.util.Random;
 import java.util.stream.IntStream;
 
 @Component
 @AllArgsConstructor
 public class Seeder implements CommandLineRunner {
 
-    private UserRepository userRepository;
-    private RoleRepository roleRepository;
-    private PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final AccountService accountService;
+    private final AccountRepository accountRepository;
+    private final TransactionRepository transactionRepository;
+    private final PasswordEncoder passwordEncoder;
 
     private final Faker faker = new Faker(Locale.forLanguageTag("es"));
+    private final Random random = new Random();
 
     @Override
     public void run(String... args) {
@@ -49,6 +58,7 @@ public class Seeder implements CommandLineRunner {
                             0
                     );
                     userRepository.save(admin);
+                    createAccountsAndTransactionsForUser(admin);
                 }
             });
 
@@ -66,11 +76,70 @@ public class Seeder implements CommandLineRunner {
                             0
                     );
                     userRepository.save(user);
+                    createAccountsAndTransactionsForUser(user);
                 }
             });
-            System.out.println("Datos de usuario creados.");
-        } else  {
-            System.out.println("Datos de usuario ya existen.");
+            System.out.println("Datos creados.");
+        } else {
+            System.out.println("Datos ya existen.");
         }
+    }
+
+    private void createAccountsAndTransactionsForUser(User user) {
+        boolean createArsAccount = random.nextBoolean();
+        boolean createUsdAccount = random.nextBoolean();
+
+        if (createArsAccount) {
+            var accountARS = new Account(
+                    CurrencyType.ARS,
+                    Constants.getTransactionLimitArs(),
+                    random.nextInt(100000),
+                    user,
+                    false,
+                    accountService.generateUniqueCbu()
+            );
+            accountRepository.save(accountARS);
+            createTransactionsForAccount(accountARS);
+        }
+
+        if (createUsdAccount) {
+            var accountUSD = new Account(
+                    CurrencyType.USD,
+                    Constants.getTransactionLimitUsd(),
+                    random.nextInt(500),
+                    user,
+                    false,
+                    accountService.generateUniqueCbu()
+            );
+            accountRepository.save(accountUSD);
+            createTransactionsForAccount(accountUSD);
+        }
+
+        if (!createArsAccount && !createUsdAccount) {
+            createAccountsAndTransactionsForUser(user);
+        }
+    }
+
+    private void createTransactionsForAccount(Account account) {
+        IntStream.range(0, 2).forEach(i -> {
+            double percentage = 0.20 + (0.50 - 0.20) * random.nextDouble();
+            BigDecimal amount = BigDecimal.valueOf(account.getBalance())
+                    .multiply(BigDecimal.valueOf(percentage))
+                    .setScale(2, RoundingMode.HALF_UP);
+            boolean addDescription = random.nextBoolean();
+
+            Transaction transaction = new Transaction(
+                    amount.doubleValue(),
+                    getRandomTransactionType(),
+                    addDescription ? faker.lorem().sentence() : "",
+                    account
+            );
+            transactionRepository.save(transaction);
+        });
+    }
+
+    private TypeTransaction getRandomTransactionType() {
+        TypeTransaction[] types = TypeTransaction.values();
+        return types[random.nextInt(types.length)];
     }
 }
