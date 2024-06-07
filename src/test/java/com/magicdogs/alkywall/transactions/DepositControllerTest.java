fde56e54bc6c8f3f08d50c29b88bc0f78e361279
maxiderecho <maxi.derecho@gmail.com>
2024-06-07 13:00:49 -0300

    Modifico una anotaciones en el DepositControllerTest

--- src/test/java/com/magicdogs/alkywall/transactions/DepositControllerTest.java
+++ src/test/java/com/magicdogs/alkywall/transactions/DepositControllerTest.java
@@ -14,10 +14,8 @@ import com.magicdogs.alkywall.servicies.TransactionService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -27,7 +25,6 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.time.LocalDateTime;
 
-@ExtendWith(MockitoExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class DepositControllerTest {
