dde79dae0b534951b70cb280a11baaa87f27135c
Vicky Lopez <vlopez@fi.uba.ar>
2024-06-12 09:44:22 -0300

    fix: origin cors

--- src/main/java/com/magicdogs/alkywall/config/WebConfig.java
+++ src/main/java/com/magicdogs/alkywall/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5173/")
+                .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true)
